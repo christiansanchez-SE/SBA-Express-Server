561cd70cc12932376787f9dbaf0f1f5e20230b6d
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-18 09:55:37 -0400

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
 
 app.listen(port, (req, res) => {
     console.log(`Listening on port: ${port}`);
